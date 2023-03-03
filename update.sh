4e5394bf17e81c1c02683d293aa87c69feef63c7
shijinsu <shijinsu888@gmail.com>
2023-03-03 17:07:44 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 git init
 git remote -v
 
-cp \d: \github\sjs\clash.yaml \d: \github \c
+cp \d: \github\sjs\clash.yaml \d: \github \ c
 cp  \sjs\clashAI.yaml csmart
 cp \sjs\v2ray.txt v
 cp \sjs\v2rayAI.txt vsmart
