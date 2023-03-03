d2bc4975cd5082502abda19c3ab0f28b745cf02f
shijinsu <shijinsu888@gmail.com>
2023-03-03 15:01:13 +0800

    update:+date +%Y-%m-%d

--- update.sh
+++ update.sh
@@ -6,7 +6,6 @@ cp clashAI.yaml csmart
 cp v2ray.txt v
 cp v2rayAI.txt vsmart
 git add .
-y="date +%Y-%m-%d"
-git commit -m "update+y" 
+git commit -m "update:"+"date +%Y-%m-%d"
 git push origin master
 read -p "Press any key to continue."
