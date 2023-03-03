911adcf367687bc0d619b9dd2d801b1dd8e38945
shijinsu <shijinsu888@gmail.com>
2023-03-03 14:56:39 +0800

    update+y

--- update - 副本.sh
+++
@@ -1,6 +0,0 @@
-git init
-cd  d:\github
-git add .
-git commit -m "update 20230303"
-git push origin master
-read -p "Press any key to continue."
--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 git init
 git remote -v
 cd  d:\github
+cp clash.yaml c
+cp clashAI.yaml csmart
+cp v2ray.txt v
+cp v2rayAI.txt vsmart
 git add .
-git commit -m "update 20230303"
+y="date +%Y-%m-%d"
+git commit -m "update+y" 
 git push origin master
 read -p "Press any key to continue."
