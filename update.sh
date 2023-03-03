90df0865e2cfe17d41d171ae00b62ef534c09c4f
shijinsu <shijinsu888@gmail.com>
2023-03-03 14:26:07 +0800

    update 20230303

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
+git init
 cd  d:\github
-read -p "Press any key to continue."
 git add .
-read -p "Press any key to continue."
 git commit -m "update 20230303"
-read -p "Press any key to continue."
 git push origin master
 read -p "Press any key to continue."
