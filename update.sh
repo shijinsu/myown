2eac4e91b47527bd1ff23e899c4091e5c572d398
shijinsu <shijinsu888@gmail.com>
2023-03-03 18:01:17 +0800

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 git init
 git remote -v
-cd  d:\github\
-cp d:\gitsource\clash.yaml  d:\github\ c1
-cp  ~.\sjs\clashAI.yaml csmart.c
-cp \sjs\v2ray.txt v
-cp \sjs\v2rayAI.txt \vsmart
 cd  d:\github
+cp  -f clash.yaml  c1
+cp  -f clashAI.yaml csmart
+cp -f v2ray.txt v
+cp -f  v2rayAI.txt  vsmart
 git add .
 git commit -m "update"
 git push origin master
