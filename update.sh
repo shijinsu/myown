fdbdc791e6b3962d23f857a0790f5f8b1cd8aaa8
shijinsu <shijinsu888@gmail.com>
2023-03-04 22:59:24 +0800

    updata

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
 git init
 git remote -v
 cd  d:\github
+cp clash.yaml c.txt
+cp clashAI.yaml cAI.txt
+cp v2ray.txt v.txt
+cp v2rayAI.txt vAI.txt
+git pull origin master
 git add .
-git commit -m "update"
+
+git commit -m "updata"
 git push origin master
 read -p "Press any key to continue."
