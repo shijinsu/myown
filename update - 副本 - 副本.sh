1643694020268696d123c6a659a0f32edf0b9b44
shijinsu <shijinsu888@gmail.com>
2023-03-05 12:37:06 +0800

    updata

--- update - 副本 - 副本.sh
+++ update - 副本 - 副本.sh
@@ -1,7 +1,7 @@
 git init
 git remote -v
 set path=d:
-cp -f \d:\githubsource\ clash.yaml d:\github\ c.txt
+cp -f \d:\githubsource\clash.yaml d:\github\ c.txt
 cp -f \d:\githubsource\ clashAI.yaml d:\github\ cAI.txt
 cp -f \d:\githubsource\ v2ray.txt d:\github\ v.txt
 cp -f \d:\githubsource\ v2rayAI.txt d:\github\ vAI.txt
