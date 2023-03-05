b133655b4a45661b5bcea02a2256d3c29dd3a838
shijinsu <shijinsu888@gmail.com>
2023-03-05 12:36:42 +0800

    updata

--- update - 副本 - 副本.sh
+++ update - 副本 - 副本.sh
@@ -1,10 +1,10 @@
 git init
 git remote -v
 set path=d:
-cp -f d:\githubsource\ clash.yaml d:\github\ c.txt
-cp -f d:\githubsource\ clashAI.yaml d:\github\ cAI.txt
-cp -f d:\githubsource\ v2ray.txt d:\github\ v.txt
-cp -f d:\githubsource\ v2rayAI.txt d:\github\ vAI.txt
+cp -f \d:\githubsource\ clash.yaml d:\github\ c.txt
+cp -f \d:\githubsource\ clashAI.yaml d:\github\ cAI.txt
+cp -f \d:\githubsource\ v2ray.txt d:\github\ v.txt
+cp -f \d:\githubsource\ v2rayAI.txt d:\github\ vAI.txt
 cd  d:\github
 git add .
 git commit -m "updata"
