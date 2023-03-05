4536cc3a6e88662351f02f645d83c9ad5fd902b3
shijinsu <shijinsu888@gmail.com>
2023-03-05 17:55:00 +0800

    updata

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@ git init
 git remote -v
 cd  d:/githubsource
 cp clash.yaml d:/github/c.txt
-cp clashAI.yaml cAI.txt
-cp v2ray.txt v.txt
-cp v2rayAI.txt vAI.txt
+cp clashAI.yaml d:/github/cAI.txt
+cp v2ray.txt d:/github/v.txt
+cp v2rayAI.txt d:/github/vAI.txt
 cd  d:/github
 git add .
 git commit -m "updata"
