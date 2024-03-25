7d949340fc45ef9a4d96f760e05e41f140be2934
shijinsu <shijinsu888@gmail.com>
2024-03-25 16:26:33 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,1 @@
-git remote -v
-cd  d:/githubsource
-cp clash.yaml d:/github/c.txt
-cp v2ray.txt d:/github/v.txt
-cd  d:/github
-git add .
-git commit -m "updata"
-git pull origin master
-read -p "Press any key to continue."
-git push origin master
-read -p "Press any key to continue."
+
